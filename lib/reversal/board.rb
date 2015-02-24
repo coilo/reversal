fe1da10cc2d6b90ad687cda7c54e8f87eaba6d7d
coilo <coilo.dev@gmail.com>
2015-02-25 01:32:09 +0900

    Add method "Reversal::Board#mount"

--- lib/reversal/board.rb
+++ lib/reversal/board.rb
@@ -7,17 +7,20 @@ module Reversal
 
   #
   class Board
-    @offset = []
+    attr_accessor :order
+
+    @offsets = []
     Offset = Struct.new(:down, :right)
 
     [-1, 0, 1].repeated_permutation(2) do |down, right|
       next if down == 0 && right == 0
-      @offset << Offset.new(down, right)
+      @offsets << Offset.new(down, right)
     end
 
     ##
     def initialize(board_size = BOARD_SIZE, order = Reversal::Disc.black)
       @board_size  = board_size
+      @board_range = 0..@board_size - 1
       @order = order
 
       @board = Array.new(@board_size**2).map.with_index do |_item, index|
@@ -25,9 +28,62 @@ module Reversal
       end
     end
 
+    ##
+    def mobility_iterator!(row, col, down: down, right: right)
+      move = 0
+      loop do
+        break unless include?(row, col)
+
+        row += down
+        col += right
+
+        move += 1
+
+        disc = yield get_disc(row, col), move
+        set_disc(row, col, disc)
+      end
+    end
+
+    ##
+    def mount(row, col)
+      return nil unless include?(row, col)
+
+      ##
+      after = before = self
+      Board.offsets.each do |offset|
+        temp = before.dup
+        temp.mobility_iterator!(row, col, down: offset.down, right: offset.right) do |item, move|
+          case item
+          when Reversal::Disc.empty
+            break
+          when @order
+            after = temp if move > 1
+            break
+          end
+          item.reverse
+        end
+      end
+
+      return nil unless after != before
+
+      ##
+      after[row, col], after.order = @order, @order.reverse
+      after
+    end
+
+    ##
+    def [](row, col)
+      get_disc(row, col)
+    end
+
+    ##
+    def []=(row, col, value)
+      set_disc(row, col, value)
+    end
+
     private
 
-    #
+    ##
     def disc_set(index)
       center = (@board_size / 2).ceil
       range  = center - 1..center
@@ -38,6 +94,24 @@ module Reversal
       else
         Reversal::Disc.empty
       end
+    end
+
+    ##
+    def include?(row, col)
+      @board_range.include?(row) && @board_range.include?(col)
+    end
+
+    #
+    def get_disc(row, col)
+      @board[row * @board_size + col]
+    end
+
+    def set_disc(row, col, value)
+      @board[row + @board_size + col] = value
+    end
+
+    class << self
+      attr_reader :offsets
     end
   end
 end
